c018cf36dcb109fc4044be41cb21a09578f0a276
Tamma-alt <84251673+Tamma-alt@users.noreply.github.com>
2023-10-26 04:21:22 +0100

    Challenge Day One

---
+++ DayOne/datatypes.js
@@ -0,0 +1,12 @@
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+let firstName = "Tamara"
+console.log(typeof firstName)
+
+console.log (typeof 23)
+
+console.log(typeof true)
+
+console.log(typeof null)
+
+console.log(typeof undefined)
---
+++ DayOne/variables.js
@@ -0,0 +1,39 @@
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let name = "Wilfred"
+let smart = true
+let career = undefined
+let cars = null
+
+// 6. Declare four variables without assigning values
+let hostel;
+let number;
+let blocks;
+let series;
+
+
+// 7. Declare four variables with assigned value
+let room = 401
+let numberOfStudents = 7
+let bunks = "four"
+let pairs = "two"
+
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstName = "Itemearau"
+let LastName = "ThankGod"
+let maritalStatus = "single"
+let country = "Nigeria"
+let age = 23
+
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let firstNam = "Itemearau", LastNam = "ThankGod", maritalStats = "single", countryOfOrigin = "Nigeria", currentAge = 23;
+
+
+// 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+myAge = 23
+yourAge = 30
+
+console.log(`I am ${myAge} years old. You are ${yourAge} years old`)
